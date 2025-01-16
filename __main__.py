dd7d9add466b79151790302bf96b7d5473eece83
Ruslan Babayev <rus.babaef@yandex.ru>
2025-01-16 13:33:53 +0300

    - remove dirs, update requirements.txt

--- __main__.py
+++ __main__.py
@@ -3,4 +3,9 @@ from data_imputer import DataImputer
 dataset_path = "data_imputer/simulations/living_building_spb_140822_080633.geojson"
 imputer = DataImputer(dataset_path)
 imputer.add_neighbors_features()
+
+# if you want to create new models
 full_data = imputer.multiple_imputation(save_models=True, save_logs=True)
+
+# if you already have fitted models (you need to put the path to the model in config.prediction.json)
+full_data = imputer.impute_by_saved_models()
